c3951cea203dadc3de44b2b57d81d9f293005407
Denis <denverm@yandex.ru>
2022-09-28 23:31:30 +0300

    Add Task13

---
+++ Task13.py
@@ -0,0 +1,21 @@
+# Напишите программу, которая принимает на вход вещественное число
+# и показывает сумму его цифр.
+# Пример:
+# - 6782 -> 23
+# 0,56 -> 11
+
+numberN = float(input("Введите число N: "))
+temp = 1
+
+while temp > 0:
+    numberN = round(numberN*10, 4)
+    temp = numberN % 10
+
+numberN = int(numberN/10)
+sum = 0
+
+while numberN > 1:
+    sum = sum + int(numberN % 10)
+    numberN /= 10
+
+print(sum)
